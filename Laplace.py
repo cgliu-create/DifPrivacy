20fb3867e1bf3013869382f8ec92f8016b6c0adb
cgliu-create <chrisliu2438@gmail.com>
2021-12-06 10:44:34 -0500

    Create Laplace.py
    global df

---
+++ Laplace.py
@@ -0,0 +1,90 @@
+import math
+import random
+import numpy as np
+
+"""
+Suppose we want to know the average proportion of some behavior.
+But, for some reason we don't want to tell the true average to some third party.
+Therefore, we decide to add a randomized error amount with a known distribution.
+This allows us hide the exact number while also allowing the other party to know that 
+it is in a certain range with a level of confidence.
+
+We like to work with values between 0 and 1.
+"""
+
+
+def randomProportion():
+    return random.random()
+
+
+n = input("Enter n, the size of the sample: \n")
+
+nums = []
+for i in range(int(n)):
+    x = randomProportion()
+    nums.append(x)
+    print(f"Individual {i+1} has {x}")
+
+trueavg = sum(nums) / len(nums)
+print(f"The true average proportion is {trueavg}")
+
+"""
+Determining Sensitivity
+∆f = Max d,d'|f(d)−f(d')| over all neighboring databases d and d’
+Since we are taking the average of a dataset, what is the max change from removing one individual?
+∆f = 1/n
+
+x is between 0 and 1, ∑x is between 0 and ∞. 
+Looking at the extremes:
+(0 + 0)/n - 0/(n-1) = 0
+(0 + 1)/n - 0/(n-1) = 1/n
+(∞ + 0)/n - ∞/(n-1) = 0
+(∞ + 1)/n - ∞/(n-1) = 0
+"""
+
+"""
+Laplace mechanism
+F(d) = f(d) + Lap(b)
+f(d) is the true value of some function of a dataset, ex: average
+Lap(b) is randomized error amount with a Laplace distribution
+Laplace has a probability density function of y = 1 / 2b * exp( - |x| / b)
+b = ∆f / ε, scale coef = sensitivity / privacy parameter
+E(x) = 0, expected error amount
+Var(x) = 2 * b ^2, variance of error dist
+
+Decreasing ε, increases b, increasing the variance, making the dist wider and less steep
+A smaller ε will yield better privacy but a less accurate response.
+"""
+b = (1/len(nums)) / 1
+noisyavg = trueavg + np.random.default_rng().laplace(0, b)
+print(f"Laplace RV for noise: center=0, scale={b}")
+print(f"Noisy average: {noisyavg}")
+
+variance = 2 * math.pow(b, 2)
+stdev = math.sqrt(variance)
+
+print(f"variance: {variance}")
+print(f"stdev: {stdev}")
+if (noisyavg - stdev) < trueavg < (noisyavg + stdev):
+    print("true average is within 1 stdev from noisy average")
+elif (noisyavg - 2*stdev) < trueavg < (noisyavg + 2*stdev):
+    print("true average is within 2 stdev from noisy average")
+elif (noisyavg - 3*stdev) < trueavg < (noisyavg + 3*stdev):
+    print("true average is within 3 stdev from noisy average")
+else:
+    print("true average is not within 3 stdev from noisy average")
+
+"""
+Differential Privacy with Laplace
+The differential privacy formula means that if the data of one individual is removed, the probability of the output with the dataset is close to the probability of the same output with the dataset that still has the individual. 
+
+Probability of a specific output, o: 
+= 1 / 2b * exp( - | f(d) - o | / b), with expected value of dataset d 
+= 1 / 2b * exp( - | f(d') - o | / b), with expected value of dataset d' (with one individual removed)
+dividing these two expressions: 
+= exp( |f(d) − f(d')| / b )
+|f(d) − f(d')|  = ∆f 
+b = ∆f / ε
+so exp( |f(d) − f(d')| / b ) = exp(ε) 
+The ratio of the two probabilities ≤ exp(ε) which fulfills the conditions for ε-Differential Privacy
+"""
